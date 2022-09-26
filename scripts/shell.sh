c2fe3df49135ec5e23f495969972810b49b2eb36
BernardoGandolpho <begandolpho@gmail.com>
2022-09-26 19:10:01 -0300

    teste

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -3,4 +3,4 @@ echo "Inicio da instalação"
 echo email
 sudo apt-get install mailutils
 echo "Fim da instalação"
-echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa" ${EMAIL}
+echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa" pedro.piccichelli@gmail.com
